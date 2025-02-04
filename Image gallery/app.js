22ee52d17362905a03bbf8d93c822985dd202f2c
My Name <emadkhan.yousufzai@gmail.com>
2025-02-04 18:03:31 +0500

    workin image gallery

--- Image gallery/app.js
+++ Image gallery/app.js
@@ -3,11 +3,44 @@ const fullimage = document.getElementById('fullimg');
 const leftKey = document.getElementById('leftKey')
 const rightKey = document.getElementById('rightKey')
 const images = document.querySelectorAll('.image-gallery img')
+const fileInput = document.getElementById('fileInput')
+const imageGallery = document.querySelector('.image-gallery')
+const dotContainer = document.getElementById('dotsContainer')
+
+
+fileInput.addEventListener('change',function(event){
+    const file = event.target.files[0];
+    if(file){
+        const reader = new FileReader();
+        reader.onload = function(e){
+            addImageToGallery(e.target.result);
+        }
+        reader.readAsDataURL(file);
+    }
+})
+
+// function generateDots(){
+//     dotContainer.innerHTML='';
+//     imagesArray.forEach(_, index)  {
+//         const dot = document.createElement('div');
+//         dot.classList.add('dot');
+//     }
+// }
+
+function addImageToGallery(src){
+    const div =document.createElement('div')
+    div.classList.add('overflow-hidden')
+    const img = document.createElement('img')
+    img.src = src
+    img.onclick = function(){openFullImage(img.src);};
+    div.appendChild(img);
+    imageGallery.appendChild(div)
+    imagesArray.push(src)
+}
 
 let currentindex =0;
 const imagesArray = Array.from(images).map((img) => img.src);
 
- 
 function openFullImage(pic) {
     fullimageBox.style.display = 'flex';
     currentindex = imagesArray.indexOf(pic)

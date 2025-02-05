c298223ae0c5f7d6c63211218c4c4e4316f3822a
My Name <emadkhan.yousufzai@gmail.com>
2025-02-05 16:53:29 +0500

    dynamic dots are added in bottom of image

--- Image gallery/app.js
+++ Image gallery/app.js
@@ -6,7 +6,12 @@ const images = document.querySelectorAll('.image-gallery img')
 const fileInput = document.getElementById('fileInput')
 const imageGallery = document.querySelector('.image-gallery')
 const dotContainer = document.getElementById('dotsContainer')
+const introScreen = document.getElementById('introScreen')
 
+setTimeout(()=>{
+    introScreen.style.opacity='0';
+    introScreen.style.visibility='hidden'
+},3000)
 
 fileInput.addEventListener('change',function(event){
     const file = event.target.files[0];
@@ -19,13 +24,21 @@ fileInput.addEventListener('change',function(event){
     }
 })
 
-// function generateDots(){
-//     dotContainer.innerHTML='';
-//     imagesArray.forEach(_, index)  {
-//         const dot = document.createElement('div');
-//         dot.classList.add('dot');
-//     }
-// }
+function generateDots(){
+    dotContainer.innerHTML='';
+    imagesArray.forEach((_,index)=>{
+        const dot = document.createElement('div');
+        dot.classList.add('dot')
+        if(index===currentindex)dot.classList.add('active')
+            dot.addEventListener('click',()=>showImageAtIndex(index))
+        dotContainer.appendChild(dot)
+    })
+}
+function updateDots() {
+    document.querySelectorAll('.dot').forEach((dot, index) => {
+        dot.classList.toggle('active', index === currentindex);
+    });
+}
 
 function addImageToGallery(src){
     const div =document.createElement('div')
@@ -36,6 +49,7 @@ function addImageToGallery(src){
     div.appendChild(img);
     imageGallery.appendChild(div)
     imagesArray.push(src)
+    generateDots();
 }
 
 let currentindex =0;
@@ -45,13 +59,16 @@ function openFullImage(pic) {
     fullimageBox.style.display = 'flex';
     currentindex = imagesArray.indexOf(pic)
     fullimage.src = pic;
+    updateDots();
 }
 function closeFullImage(){
     fullimageBox.style.display = 'none';
+    updateDots();
 }
 function showNextImage(){
     currentindex = (currentindex + 1) % imagesArray.length;
     fullimage.src = imagesArray[currentindex];
+    updateDots()
     rightKey.style.boxShadow = "0px 0px 45px rgba(255, 255, 255, 0.5)";
     setTimeout(() => {
         rightKey.style.boxShadow = "0px 0px 5px rgba(255, 255, 255, 0.3)";
@@ -59,10 +76,13 @@ function showNextImage(){
     setTimeout(() => {
         rightKey.style.boxShadow = "0px 0px 5px rgba(255, 255, 255, 0.3)";
     }, 500);
+    
 }
 function showPreviousImage(){
     currentindex = (currentindex - 1 + imagesArray.length) % imagesArray.length;
     fullimage.src = imagesArray[currentindex];
+    updateDots();
+
     leftKey.style.boxShadow = "0px 0px 45px rgba(255, 255, 255, 0.5)";
     setTimeout(() => {
         leftKey.style.boxShadow = "0px 0px 5px rgba(255, 255, 255, 0.3)";
@@ -71,6 +91,12 @@ function showPreviousImage(){
         leftKey.style.boxShadow = "0px 0px 5px rgba(255, 255, 255, 0.3)";
     }, 500);
 }
+function showImageAtIndex(index){
+ currentindex=index;
+ fullimage.src=imagesArray[currentindex];
+ updateDots()
+}
+
 rightKey.addEventListener("click", showNextImage);
 leftKey.addEventListener("click", showPreviousImage);
 
@@ -81,3 +107,5 @@ document.addEventListener("keydown",(event)=>{
         if(event.key === 'Escape') closeFullImage();
     }
 })
+generateDots();
+

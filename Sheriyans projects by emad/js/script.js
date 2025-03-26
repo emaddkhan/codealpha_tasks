6620b7dba20cac390b2cb59e1af0950606a74a6a
My Name <emadkhan.yousufzai@gmail.com>
2025-03-26 14:27:39 +0500

    3/26/2025

---
+++ Sheriyans projects by emad/js/script.js
@@ -0,0 +1,14 @@
+// variable and constants 
+// variable and constant dono aik container hai mgr agr hum variable me jo data save krein woh hum baad me change kr skte hain magr agr hum 
+// constant me data save krein toh woh hum baat me change nhi kr skte
+
+//intrepater and compiler
+// interepator or compiler dono translater hai jo hamre english code ko 0 or 1 me translate krtein hain or dono me ye fark hai k
+// intrepater task jaldi solve krta hain or run slow krta hai
+// or compiler task deir se solve krta hai or run deir se krta hai
+// Js by default intrepator language hai
+// JIT compiler woh compiler hai jo dono ki power use krta hai yani intepator ki jaldi solve krne wali or compiler ki jaldi run krne wali
+
+//windows
+// js kuch function jo brrowsers se borrow krta hai woh functions windows kehlate hain by default woh js language ka hiss nhi hote for example
+// alert, console, prompt etc

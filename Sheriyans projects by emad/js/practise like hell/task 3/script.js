9ae1866d4e6408df27f22b26d698ac5f0d7e20ef
My Name <emadkhan.yousufzai@gmail.com>
2025-04-08 16:37:08 +0500

    4/8/2025

--- Sheriyans projects by emad/js/practise like hell/task 3/script.js
+++ Sheriyans projects by emad/js/practise like hell/task 3/script.js
@@ -1,17 +1,31 @@
-const form = document.querySelector('form');
-const inps =document.querySelectorAll('input[type="text"]')
-const h4 = document.querySelector("h4")
-
-form.addEventListener("submit",function(e){
+const h4 = document.querySelector("h4");
+const inputs = document.querySelectorAll("input[type='text']");
+const button = document.querySelector("button");
+const para = document.querySelector("para")
+const emailpara = document.querySelector('#email')
+const namepara = document.querySelector("#name")
+const inp1= document.querySelector('#inp1')
+const inp2= document.querySelector('#inp2')
+button.addEventListener("click",function(e){
     e.preventDefault();
-    for(var i = 0; i<inps.length; i++){
-        if(inps[i].value.trim()===""){
-            h4.textContent="fill the form";
-            h4.style.color="red";
-            break;
-        }
-        else{
+    if(inp1.value===""){
+        emailpara.textContent="give email"
+    }else{
+        emailpara.textContent=""
+    }
+    if(inp2.value===""){
+        namepara.textContent="give name"
+    }else{
+        namepara.textContent=""
+    }
+    for(i=0;i<inputs.length;i++){
+        if(inputs[i].value===""){
+            h4.textContent = "fill the form";
+            para.textContent="fill the email and name"
+            para.style.color = "red";
+        }else{
             h4.textContent="";
+            para.textContent = "";
         }
     }
 })

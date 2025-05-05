1b6c3686bbe0aeb9a8f8eb5c9f67038624e8818c
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-05-05 17:07:50 +0500

    learning react...

--- Sheriyans projects by emad/React Js/lecture/script.js
+++ Sheriyans projects by emad/React Js/lecture/script.js
@@ -15,14 +15,35 @@
 // var state = [1,2,3,4,5]
 // state=[1,2,3]
 
-// ----SPREAD OPERATER----
+// ----SPREAD OPERATER----***
 //spread operater array ko copy krne k liye use hota hai
 //copy krne ka trika ye hai k me woh bracket likhoo ga jisse me copy krna chata hoo uske ander teen dotss lgaoo ga ...or uske sath array
 //ka name likh doo ga jisko copy krna hai
-var arr = [1,2,3,4,5];
-var copy = [...arr];
-copy.pop();
+// var arr = [1,2,3,4,5];
+// var copy = [...arr];
+// copy.pop();
 
-var state = {name:"emad",age:"18"}
-var data = {...state}
-data.age="19"
+// var state = {name:"emad",age:"18"}
+// var data = {...state}
+// data.age="19"
+// state=data;
+
+//----DESTRUCTURING----***
+// var obj ={name:"harsh",age:18,email:"lalalalalalalala@gmail.com"}
+// const {age} = obj;
+//another example
+// var obj = {name:"jja",social:{
+//     facebook:{
+//         first:"hah",
+//         sec:"jjdj",
+//     }
+// }}
+// now without destructing i want to accese first
+// obj.social.facebook.first
+// // with destructing
+// const {first}=obj.social.facebook;
+//or haan app name bhi de skte ho destruct krte hue kese k
+var arr=[12,function(){},33];
+// var [first,sec]=arr; //iska matlab ye k arr me jo 12 hai usko mene first ka name de dia or jo function hai mene usko sec name dedia crazy nah
+// agr mujy arr ka pehla or teesra element nikalna hai toh woh aese krenge
+var [chacha,,chachi]=arr;//chacha me pehli value or chachi me teesri value agi or mujy array ki dusri value nhi chahiye the tho use men blank chordia

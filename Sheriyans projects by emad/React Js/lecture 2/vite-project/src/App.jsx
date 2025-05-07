37e8a77f72abc78bffdb048d2fc16c1097bcfbc9
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-05-07 22:39:32 +0500

    learning react ....... and project of rendering card through data

--- Sheriyans projects by emad/React Js/lecture 2/vite-project/src/App.jsx
+++ Sheriyans projects by emad/React Js/lecture 2/vite-project/src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Card from "./Components/Card";
-function App(){
+function App() {
   return (
-   <div>
-    <Card></Card>
-   </div>
-  )
+    <>
+      <Card/>
+    </>
+  );
 }
 export default App;
+
+//aik warning ati hai k each child should have its unique number toh ye islia hota hai k react apna aik virtual dom bnata hai to jb bhi map
+//se elem ate hai toh usko sb same lgte toh ise resolve krne ka trika ye hai k hum child ko aik key de de yani map k andr jo function hota
+//use do param pass krayenge aik toh value hogi or dosra index kiu k index hr bar change hota hai or key k andr bhi index de dein gai
+
+
+//Fragment
+//fragment basically ye hai k ap bina extra div bnye ap cheezon ko wrap kr skte ho kuch yoo <></>
+
--- Sheriyans projects by emad/React Js/lecture 2/vite-project/src/Components/Card.jsx
+++ Sheriyans projects by emad/React Js/lecture 2/vite-project/src/Components/Card.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React from "react";
 
 function Card() {
+  const data = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1742240216264-f0aac25ef4ba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
+      name: "Laptop Wala",
+      description:
+        "The internet’s source for visuals.Powered by creators everywhere.",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1743385779331-487e8391bf08?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
+      name: "Books",
+      description:
+        "The internet’s source for visuals.Powered by creators everywhere.",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1744360820043-59b729149f7d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D",
+      name: "Car on road",
+      description:
+        "The internet’s source for visuals.Powered by creators everywhere.",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1726607288637-a646ddd3814a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D",
+      name: "Mobile Payment",
+      description:
+        "The internet’s source for visuals.Powered by creators everywhere.",
+    },
+  ];
   return (
-    <div className='w-full h-screen bg-zinc-200'>
-      <div className='w-52 bg-zinc-100 absolute left-1/2 top-1/2 -translate-y-[50%] -translate-x-[50%] rounded-md overflow-hidden'>
-      <div className='w-full h-32 bg-zinc-300'>
-        <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YW1hem9ufGVufDB8fDB8fHww" alt="" />
-      </div>
-      <div className='w-full px-3 py-4'>
-        <h1 className='font-semibold'>Amazon Basics.</h1>
-        <p className='text-xs mt-3 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem sit id!</p>
-      </div>
-      </div>
+    <div className="w-full flex items-center justify-center gap-10 h-screen bg-zinc-200">
+      {data.map((elem,index) => (
+        <div key={index} className="w-52 bg-zinc-100 rounded-md overflow-hidden">
+          <div className="w-full h-32 bg-zinc-300">
+            <img
+              className="w-full h-full object-cover"
+              src={elem.image}
+              alt=""
+            />
+          </div>
+          <div className="w-full px-3 py-4">
+            <h1 className="font-semibold">{elem.name}.</h1>
+            <p className="text-xs mt-3 ">
+              {elem.description}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;

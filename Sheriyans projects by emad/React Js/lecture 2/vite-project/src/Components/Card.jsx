7eddc8158e9202ef5ce25a026f9606a32b5f427c
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-05-08 17:48:46 +0500

    learning events handling in react

--- Sheriyans projects by emad/React Js/lecture 2/vite-project/src/Components/Card.jsx
+++ Sheriyans projects by emad/React Js/lecture 2/vite-project/src/Components/Card.jsx
@@ -1,60 +1,33 @@
+import { data } from "autoprefixer";
 import React from "react";
 
 function Card() {
   const data = [
     {
-      image:
-        "https://images.unsplash.com/photo-1742240216264-f0aac25ef4ba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
-      name: "Laptop Wala",
-      description:
-        "The internet’s source for visuals.Powered by creators everywhere.",
+      name: "fark hai",
+      description: "the antiboy yourbody everybody say chava chava",
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1743385779331-487e8391bf08?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
-      name: "Books",
-      description:
-        "The internet’s source for visuals.Powered by creators everywhere.",
-      instock: true,
+      name: "Aur",
+      description: "the antiboy yourbody everybody say o re piya o re piya",
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1744360820043-59b729149f7d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D",
-      name: "Car on road",
-      description:
-        "The internet’s source for visuals.Powered by creators everywhere.",
-      instock: false,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1726607288637-a646ddd3814a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D",
-      name: "Mobile Payment",
-      description:
-        "The internet’s source for visuals.Powered by creators everywhere.",
-      instock: true,
+      name: "Sham too",
+      description: "the antiboy yourbody everybody say sham too tarif me",
     },
   ];
+  const downloadHandler = ()=>{alert("hey")};
   return (
-    <div className="w-full flex items-center justify-center gap-10 h-screen bg-zinc-200">
+    <div className="w-full h-screen bg-zinc-300 flex flex-col justify-center items-center gap-4">
       {data.map((elem, index) => (
-        <div
-          key={index}
-          className="w-52 bg-zinc-100 rounded-md overflow-hidden"
-        >
-          <div className="w-full h-32 bg-zinc-300">
-            <img
-              className="w-full h-full object-cover"
-              src={elem.image}
-              alt=""
-            />
-          </div>
-          <div className="w-full px-3 py-4">
-            <h1 className="font-semibold">{elem.name}.</h1>
-            <p className="text-xs mt-3 ">{elem.description}</p>
-            <button className={`px-2 py-1 ${elem.instock ? "bg-blue-600" :"bg-red-600"} rounded-sm text-zinc-100 mt-4 text-xs`}>
-              {elem.instock ? "In stock" : "Out of Stock"}
-            </button>
-          </div>
+        <div key={index} className="w-80 song px-3 py-2 bg-zinc-100 rounded-md">
+          <h3 className="font-semibold text-xl">{elem.name}</h3>
+          <p className="text-xs mt-2">
+            {elem.description}
+          </p>
+          <button onClick={downloadHandler} className="px-2 py-1 rounded bg-green-600 text-zinc-50 mt-2 font-semibold">
+            Downlod Now
+          </button>
         </div>
       ))}
     </div>
@@ -62,6 +35,3 @@ function Card() {
 }
 
 export default Card;
-
-//bhai class me agr logic liknhi ho toh sari classes select kro or unko {}me rkhdo or take hum string pass kr ske toh ``back tag k undr 
-//classes rkhein os jis class k sath khelna hai ${} toh iske undr logic likhain ge or wohh class pass krdein ge 

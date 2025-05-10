c9bdb3c49b06b81fc0915bb136cdb86cc0ce0a26
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-05-10 22:50:28 +0500

    mastering usetate made a small imag shuffling project using useState

--- Sheriyans projects by emad/React Js/lecture 2/vite-project/src/App.jsx
+++ Sheriyans projects by emad/React Js/lecture 2/vite-project/src/App.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import Card from "./Components/Card";
+import { FaArrowRightLong } from "react-icons/fa6";
 function App() {
-  const [val,setVal] =useState(true)
+  const [val,setVal] =useState(false)
   return (
     <>
-      <div className="p-4 bg-zinc-300 h-screen w-full">
-        <h1>{val?"dont go":"go out"}</h1>
-        <button onClick={()=>setVal(()=>!val)} className={`py-1 px-2 text-white ${val?"bg-blue-500":"bg-red-500"} rounded-full font-semibold`}>change</button>
+      <div className="p-4 bg-zinc-300 flex items-center justify-center h-screen w-full">
+       <div className="w-80 overflow-hidden flex h-44 relative bg-zinc-400 rounded-md">
+        <img className={`flex shrink-0 h-full w-full duration-300 ease-in-out object-cover ${val===false?"translate-x-[0%]":"-translate-x-[100%]"}`} src="https://images.unsplash.com/photo-1746469410708-493e86266c05?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt="" />
+        <img className={`flex shrink-0 h-full w-full duration-300 ease-in-out object-cover ${val===false?"translate-x-[0%]":"-translate-x-[100%]"}`} src="https://images.unsplash.com/photo-1746768934151-8c5cb84bcf11?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8" alt="" />
+        <span onClick={()=>setVal(()=>!val)} className={`h-10 w-10 bg-[#dadada9b] duration-200 -translate-x-[50%] ${val?"rotate-180":"rotate-0"} left-1/2 rounded-full flex items-center justify-center absolute bottom-[10%]`}>
+          <FaArrowRightLong size={"1.2em"}/>
+        </span>
+       </div>
       </div>
     </>
   );

69be709cc95660239069838f1ca31d830d7ec582
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-05-14 16:50:34 +0500

    learning how to apply styles on Navlink in react

--- Sheriyans projects by emad/React Js/lecture 4/vite-project/src/components/Nav.jsx
+++ Sheriyans projects by emad/React Js/lecture 4/vite-project/src/components/Nav.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className='flex justify-center gap-10 py-4'>
-            <Link to="/">Home</Link>
-            <Link to="/user">User</Link>
-            <Link to="/about">About</Link>
-        </nav> 
-  )
+    <nav className="flex justify-center gap-10 py-4">
+      <NavLink
+        className={(e) => {
+          return [
+            e.isActive ? "text-red-200" : "",
+            e.isActive ? "font-bold" : "",
+          ].join(" ");
+        }}
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={(e) => {
+          return [
+            e.isActive ? "text-red-200" : "",
+            e.isActive ? "font-bold" : "",
+          ].join(" ");
+        }}
+        to="/user"
+      >
+        User
+      </NavLink>
+      <NavLink
+        className={(e) => {
+          return [
+            e.isActive ? "text-red-200" : "",
+            e.isActive ? "font-bold" : "",
+          ].join(" ");
+        }}
+        to="/about"
+      >
+        About
+      </NavLink>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;

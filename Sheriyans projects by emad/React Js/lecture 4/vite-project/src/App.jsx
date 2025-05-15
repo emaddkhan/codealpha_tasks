5c64c4e178c00696f5c488688df20a22334ccdfb
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-05-15 22:04:52 +0500

    learninp api integration in react

--- Sheriyans projects by emad/React Js/lecture 4/vite-project/src/App.jsx
+++ Sheriyans projects by emad/React Js/lecture 4/vite-project/src/App.jsx
@@ -1,19 +1,37 @@
+import axios from "axios";
 import React from "react";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import User from "./components/User";
-import Routing from "./utils/Routing";
-import Nav from "./components/Nav";
 
 function App() {
-  
+  const api = "https://fakestoreapi.com/products";
+  const getProducts = () => {
+    axios.get(api)
+    .then(data=>{
+      console.log(data)
+    })
+  };
+  const addProducts = () => {
+    axios.post(api,{
+    title: "Emads Iphone",
+    brand: "Apple",
+    model: "Apple vision pro First Gen",
+     color: "Black",
+    category: "appliances",
+    discount: 1
+  })
+    .then(data=>{
+      console.log(data)
+    })
+  };
   return (
     <>
-      <Nav />
-      <Routing/>
+    <div className="p-5">
+    <button onClick={getProducts} className="px-4 py-1 rounded-md bg-red-300 text-white">Get Products</button>
+    <br /><br />
+    <button onClick={addProducts} className="px-4 py-1 rounded-md bg-red-300 text-white">Add New Products</button>
+
+    </div>
     </>
-  );
+  )
 }
 
 export default App;

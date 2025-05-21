7a7b4120ac13036645dad7c631fe4561cb71262b
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-05-21 21:46:08 +0500

    learning Context Api in React

--- Sheriyans projects by emad/React Js/lecture 4/vite-project/src/components/Nav.jsx
+++ Sheriyans projects by emad/React Js/lecture 4/vite-project/src/components/Nav.jsx
@@ -6,7 +6,6 @@ function Nav() {
     <div className='flex justify-center p-5 gap-10'>
         <Link className='font-semibold' to="/">Home</Link>
         <Link className='font-semibold' to="/user">User</Link>
-        <Link  className='font-semibold' to="/userdetail">User Details</Link>
         <Link  className='font-semibold' to="/about">About</Link>
     </div>
   )
--- Sheriyans projects by emad/React Js/lecture 4/vite-project/src/components/UserDetail.jsx
+++ Sheriyans projects by emad/React Js/lecture 4/vite-project/src/components/UserDetail.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { useContext } from 'react'
 import { UserContext } from '../context/Context'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function UserDetail() {
+  const navigate=useNavigate();
+  const BackHandler=()=>{
+    navigate(-1)
+  }
   const {id}=useParams()
   const {user}=useContext(UserContext)
   return (
     <div className='w-1/2 mx-auto mt-10'>
-      <h1>{user[id].name}</h1>
+      <h1 className='text-4xl text-red-400 font-semibold'>{user[id].name}</h1>
+      <h1 className='text-2xl text-red-300 font-semibold'>{user[id].place}</h1>
+      <button onClick={BackHandler} className='px-3 py-2 bg-red-500 rounded-md text-white mt-4'>Go Back</button>
     </div>
   )
 }

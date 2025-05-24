003d2e974079aef7c609d9b54d187535e60f31d5
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-05-24 23:04:40 +0500

    category page completed

--- Sheriyans projects by emad/React Js/warmup project/project 1/src/components/Home.jsx
+++ Sheriyans projects by emad/React Js/warmup project/project 1/src/components/Home.jsx
@@ -7,28 +7,36 @@ import axios from "../utils/axios.jsx";
 
 function Home() {
   const [product, setProducts] = useContext(productData);
+  const [loading,setLoading]=useState(false);
   const { search } = useLocation();
   const category = search.split("=")[1];
   console.log(category);
-  const[filteredProducts,setFilteredProducts]= useState(product)
+  const[filteredProducts,setFilteredProducts]= useState(null)
   const getCategoryProducts =async () => {
     try {
+      setLoading(true)
       const {data} =await axios.get(`products/category?type=${category}`)
       console.log(data.products)
       setFilteredProducts(data.products)
+      setLoading(false)
     } catch (error) {
       console.log(error);
     }
   };
   useEffect(()=>{
-    if(category) getCategoryProducts();
-  },[category])
+    if(!filteredProducts||category!="undefined") setFilteredProducts(product)
+    if(category != undefined) getCategoryProducts();
+  },[category,product])
   console.log(filteredProducts)
   return product ? (
     <>
       <Navbar />
-      <div className="w-[85%] h-screen p-5 flex flex-wrap gap-2 overflow-x-hidden overflow-y-auto">
-        {filteredProducts &&filteredProducts.map((items) => (
+      <div className="w-[85%] h-screen p-5 flex mt-[3%] flex-wrap gap-2 overflow-x-hidden overflow-y-auto">
+        {loading ? (
+        <Loading />
+      ) : (
+        filteredProducts &&
+        filteredProducts.map((items) => (
           <Link
             key={items.id}
             to={`/details/${items.id}`}
@@ -44,7 +52,8 @@ function Home() {
               {items.title}
             </p>
           </Link>
-        ))}
+        ))
+      )}
       </div>
     </>
   ) : (
--- Sheriyans projects by emad/React Js/warmup project/project 1/src/components/Navbar.jsx
+++ Sheriyans projects by emad/React Js/warmup project/project 1/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
 import { productData } from "../utils/Context";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
   const [product, setProducts] = useContext(productData);
+  const {search,pathname}=useLocation();
   let distinctProducts =
     product && product.reduce((acc, cv) => [...acc, cv.category], []);
   distinctProducts = [...new Set(distinctProducts)];
@@ -15,6 +16,7 @@ function Navbar() {
 //  console.log(color())
   return (
     <div className="h-screen flex flex-col items-center py-5 w-[15%] px-5 bg-zinc-100">
+      {pathname!='/'||search.length>0?(<Link to="/" className="bg-zinc-300 px-3 py-1 rounded-md text-white absolute left-[16.5%] top-[3.5%]">Home</Link>):""}
       <button className="px-5 py-3 border">Add New Category</button>
       <hr className="w-[90%] bg-zinc-300 h-[2px] my-5" />
       <h1 className="w-full font-semibold text-2xl">Category Filter</h1>

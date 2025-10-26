8c17cd3709d1ec777af1bddf5fdd2deade677b07
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-10-26 22:03:39 +0500

    learninG C language

---
+++ C language/Hello.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+int main(){
+    int number =25;
+    char star ="*";   
+    int age="18";
+    age=20;
+    float pi= 3.142;
+
+    int a=20;
+    int A=40;
+
+    int _age=18;
+    int final_price=1800;
+    return 0;
+}

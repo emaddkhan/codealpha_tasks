0d602c39a03f888cbdabbe9a6a538457ec42558a
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-10-28 21:49:12 +0500

    learning C language

--- C language/Hello.c
+++ C language/Hello.c
@@ -1,16 +1,19 @@
 # include<stdio.h>
 
 int main(){
-    int number =25;
-    char star ="*";   
-    int age="18";
-    age=20;
-    float pi= 3.142;
+    // int age = 18;
+    // float pi=3.142;
+    // char hastags ="#";
+    // printf("Hello world\n");
+    // printf("Hello world\n");
+    // printf("Hello world\n");
+    // printf("Hello world\n");
+    // int age =18;
+    // printf("age is%d",age);
+    // float pi =3.14;
+    // printf("value of pi is %f",pi);
+    char star ='*';
+    printf("character is star %c",star);
 
-    int a=20;
-    int A=40;
-
-    int _age=18;
-    int final_price=1800;
     return 0;
 }

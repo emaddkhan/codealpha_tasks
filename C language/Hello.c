c31258a0d337275c38fbe7c38156eb065ed3e313
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-10-29 22:02:25 +0500

    learning c language

--- C language/Hello.c
+++ C language/Hello.c
@@ -12,8 +12,44 @@ int main(){
     // printf("age is%d",age);
     // float pi =3.14;
     // printf("value of pi is %f",pi);
-    char star ='*';
-    printf("character is star %c",star);
+    // char star ='*';
+    // printf("character is star %c",star);
+    // int age;
+    // printf("enter age");
+    // scanf("%d",&age);
+    // printf("age is %d",age);
+    // int a,b;
+    // printf("enter A");
+    // scanf(" %d",&a);
+    // printf("Enter B");
+    // scanf("%d",&b);
+    // printf("total is %d",a + b);
+    // return 0;
+
+    //QUESTIONS:
+    //write a program to calculate area of a square
+    //Answere:
+    // int side;
+    // printf("Enter square side");
+    // scanf("%d",&side);
+    // printf("Area of a square is %d",side*side);
+    // if value is not integer
+    // float side;
+    // printf("Enter side");
+    // scanf("%f",&side);
+    // printf("Area of a square is %f",side*side);
+    // return 0;
+
+
+    //QUESTION 02;
+    //Calculate the area of a circle
+    //Answere : as we know area of  circle is pi(r)square
+    float pi = 3.142;
+    float radius;
+    printf("Enter radius of a circle");
+    scanf("%f",&radius);
+    float area = pi * radius * radius;
+    printf("Area of a circle is %f ",area);
 
     return 0;
 }

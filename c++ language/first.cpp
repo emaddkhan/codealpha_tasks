571c11df23ef45e8a489e13949d4648a9001b92b
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-11-13 21:26:45 +0500

    learning c++ langauage data types, type casting,cin

--- c++ language/first.cpp
+++ c++ language/first.cpp
@@ -1,7 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"hello world"<<endl;
-    cout<<"hello world";
+int main()
+{
+    // int a = 25;
+    // cout <<sizeof(a)<<endl;  //sizeOf is a funtion which is used to find the number of bytes covering by the int
+    // char grade = 'A';
+    // cout<<sizeof(grade)<<endl;
+    // bool isSafe = true;
+    // cout << isSafe << endl;
+
+    // char grade ='A';
+    // int value=grade;
+    // cout<<value<<endl;
+
+    // int price=100.89;
+    // int newPrice =(int)price;
+    // cout<<newPrice<<endl;
+    // int age;
+    // cout<<"Enter your age: ";
+    // cin>>age;
+    // cout<<"Your age is :"<<age<<endl;
     return 0;
 }

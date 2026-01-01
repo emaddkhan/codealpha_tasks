9804cbc23d86a9c8933820452dd21714da111697
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-01-01 10:34:43 +0500

    learning oop in project

--- c++ language/oop/oop.cpp
+++ c++ language/oop/oop.cpp
@@ -2,22 +2,96 @@
 #include <string>
 using namespace std;
 
-class Teacher {
-    //properties;
+class Teacher //ye aik encapsulation hai
+{ // its a obj
+    // properties;
+private: //this can acces inside class and to derived classes
 
+    double salary;
+
+public:  //this can access to every one 
     string name;
     string dept;
     string subject;
-    double salary;
-
-    //methods/member funtion;
-    void changeDept(string newDept){
-        dept=newDept;
+    //agr me chahta hoo k mujy baar baar dept set nah krna parhe hr teacher k liye toh me kia kroo ga constructer aik hi bari me set krdooga dept or woh hr teacher k liye set hojeyega
+    // Teacher(){ //non parametrizre constructer
+    //     dept="Computer Science";
+    // }
+    //parametrize constructer
+    Teacher(string name,string dept,string subject,double salary){
+        this->name=name;//this is used to show the property of object to compiler
+        this->dept=dept;
+        this->subject=subject;
+        this->salary=salary;
     }
+
+    //copy constructer
+    Teacher(Teacher &orgObj){//pass by refercne ->iska matlab yahan pe jo obj creat ho rha hai woh copy mhi balke original hi hai uska hum address pass krte hai
+        cout<<"i am a copy constructer\n";
+        this->name=orgObj.name;
+        this->dept=orgObj.dept;
+        this->subject=orgObj.subject;
+        this->salary=orgObj.salary;
+    };
+
+    // methods/member funtion;
+    void changeDept(string newDept)
+    {
+        dept = newDept;
+    }
+    //to access the private property we set function in public to get it
+    //setterFunction
+    // void setSalary(double s){
+    //     salary=s;
+    // }
+    // double getSalary(){
+    //     return salary;
+    // }
+    void getInfo(){
+        cout<<"name: "<<name<<endl;
+        cout<<"subject "<<subject<<endl;
+    }
+};
+class Bank{
+    private:
+    double balance;
+    string password; //data hiding
+    public:
+    Bank(){ //non parametrizre constructer
+        cout<<"hi\n";
+    }; //so this is the constructer that why it doesnt have any return type
+    string name;
+    string accountId;
 
 };
 
-int main(){
-    Teacher t1;// iska matlab humne hamari first teacher k liye object create krlia
+int main()
+{
+    // Teacher t1;//jese hi humne object creation wali line likhi wese hi constructer ko call ja rhi hai
+    // iska matlab humne hamari first teacher k liye object create krlia
+    // agr hame kisi object ki property ya method ko access krna hot hota hai toh hum ise
+    //.opertor ke zaria access krte hain
+    // t1.name = "emad";
+    // t1.subject="English";
+
+    //now we will use parametrize constructer
+
+    Teacher t1("Emad","Computer Science","C++",25000);
+
+    //now we can access private property
+    // t1.setSalary(25000);
+    // cout<<t1.getSalary()<<endl;
+    // cout<<t1.name<<endl;
+    // t1.getInfo();
+    Teacher t2(t1);//default copy constructer invoke
+    t1.getInfo();
+
+    Bank b1;
+    Bank b2; //yani by default constructer run nhi hoga balke hamra wala hoga woh bhi sb se pehle
+    b1.name="ching chong";
+    b1.accountId="@568fds";
+    cout<<b1.name<<endl;
+    cout<<b1.accountId<<endl;
+
     return 0;
 }

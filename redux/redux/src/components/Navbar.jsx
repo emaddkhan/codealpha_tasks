0df230578e25716dc775c080662df228b1cfd110
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-04-04 22:01:37 +0500

    something

---
+++ redux/redux/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../assets/Logo.png';
+
+function Navbar() {
+    
+  return (
+    <nav className='w-[60%] overflow-hidden absolute p-[1.5vw] top-[-4vw] left-[19vw] flex items-center justify-between h-[8vw] rounded-[8vw] bg-white'>
+        <div className='navContainer bg-red-400 h-full mt-[4vw] w-full'></div>
+      </nav>
+  )
+}
+
+export default Navbar
